78ce7c16d0f474ce5d2d4bae1e075f063b94cd58
Rahul Kumar Yadav <ry573870@gmail.com>
2026-07-23 10:55:38 +0530

    register user complete with better folder structure

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,14 +1,20 @@
 import express from "express";
 import dotenv from 'dotenv';
 import cors from "cors";
+
 import userRoutes  from "./routes/userRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
+
 dotenv.config();
+
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use("/api/users", userRoutes);
+
+
+app.use("/api/auth", authRoutes);
 
 
 app.get("/",(req,res) =>{
--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -3,21 +3,5 @@ import User from "../models/user.js";
 
 const router = express.Router();
 
-router.post("/register", async(req,res) =>{
-    try{
-        const { name, email, password } = req.body;
-
-        const user = await User.create({
-            name,
-            email,
-            password,
-        });
-        res.status(201).json(user);
-    }catch(err){
-        res.status(500).json({
-            message: err.message,
-        });
-    }
-});
 
 export default router;
