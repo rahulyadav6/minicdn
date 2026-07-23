63924eff0505efb39090c76e8bccbb23cc1a5cba
Rahul Kumar Yadav <ry573870@gmail.com>
2026-07-23 11:20:56 +0530

    login route complete

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -39,3 +39,38 @@ export const registerUser = async (req, res) => {
 
     }
 };
+
+
+export const loginUser = async (req,res)=>{
+    try{
+        const{email, password} = req.body;
+
+        // find user 
+        const user = await User.findOne({email});
+        if(!user){
+            return res.status(401).json({
+                message: "Invalid email or password",
+            });
+        }
+
+        // compare password
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if(!isMatch){
+            return res.status(401).json({
+                message: "Invalid email or password",
+            });
+        }
+
+        res.status(200).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user._id),
+        });
+    }catch(error){
+        res.status(500).json({
+            message: error.message,
+        });
+    }
+};
