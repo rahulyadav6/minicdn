7a5d427763a946a2e02f3082a3c05e52c2833716
Rahul Kumar Yadav <ry573870@gmail.com>
2026-07-23 08:01:32 +0530

    user model create

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,14 +1,19 @@
-import express from "express"
-import dotenv from 'dotenv'
-
+import express from "express";
+import dotenv from 'dotenv';
+import cors from "cors";
+import userRoutes  from "./routes/userRoutes.js";
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+app.use("/api/users", userRoutes);
+
 
 app.get("/",(req,res) =>{
     res.send("Backend running");
 })
+
 
 export default app;
--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 const connectDB = async()=>{
     try{
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log("mongodb connected");
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+        console.log(`MongoDB Connected: ${conn.connection.name}`);
     }catch(error){
-        console.log(error.mongoose);
+        console.log("MongoDB Error:", error.message);
         process.exit(1);
     }
 }
