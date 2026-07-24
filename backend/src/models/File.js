cefa281bb860ecf8b2c81192e005cdd1cebc8f07
Rahul Kumar Yadav <ry573870@gmail.com>
2026-07-24 07:06:43 +0530

    create file model

---
+++ backend/src/models/File.js
@@ -0,0 +1,43 @@
+import mongoose, { mongo } from "mongoose";
+
+const fileSchema = new mongoose.Schema(
+    {
+        filename:{
+            type: String,
+            required: true,
+        },
+        originalName:{
+            type: String,
+            required: true,
+        },
+        mimeType:{
+            type: String,
+            required: true,
+        },
+        size:{
+            type: Number,
+            required: true,
+        },
+        path:{
+            type: String,
+            required: true
+        },
+        project:{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Project",
+            required: true,
+        },
+        uploadedBy:{
+            type: mongoose.Schema.Types.ObjectId,
+            ref:"User",
+            required: true,
+        },
+    },
+    {
+        timestamps:true,
+    }
+);
+
+const File = mongoose.model("File", fileSchema);
+
+export default File
