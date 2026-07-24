f0403d5caa170b2137b37c2cfcf5f6fa0c033e63
Rahul Kumar Yadav <ry573870@gmail.com>
2026-07-24 06:50:12 +0530

    delete project route complete

--- backend/src/controllers/projectController.js
+++ backend/src/controllers/projectController.js
@@ -57,3 +57,33 @@ export const getProjectById = async(req,res)=>{
         });
     }
 };
+
+export const deleteProject = async(req,res)=>{
+    try {
+        const project = await Project.findById(req.params.id);
+        if(!project){
+            return res.status(404).json({
+                message:"Project not found",
+            });
+        }
+        if(project.owner.toString() !== req.user._id.toString()){
+            return res.status(403).json({
+                message: "Access denied",
+            });
+        }
+
+        // Future:
+        // 1. Delete all files from MongoDB
+        // 2. Delete files from Origin Storage
+        // 3. Notify all Edge Servers
+
+        await project.deleteOne();
+        res.status(200).json({
+            message: "Project deleted successfully",
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: error.message,
+        });
+    }
+}
--- backend/src/routes/projectRoutes.js
+++ backend/src/routes/projectRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
 import protect from "../middleware/authMiddleware.js";
-import { createProject, getProjects, getProjectById  } from "../controllers/projectController.js";
+import { createProject, getProjects, getProjectById, deleteProject  } from "../controllers/projectController.js";
 
 const router = express.Router();
 
 router.post("/", protect, createProject);
 router.get("/", protect, getProjects);
 router.get("/:id", protect, getProjectById);
+router.delete("/:id", protect, deleteProject);
 export default router;
