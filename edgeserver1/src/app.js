2c692d255ba2e8d5cf1727b7e03afc55742c3a7d
Rahul Kumar Yadav <ry573870@gmail.com>
2026-07-25 07:23:45 +0530

    cache invalidation complete

--- edgeserver1/src/app.js
+++ edgeserver1/src/app.js
@@ -1,8 +1,11 @@
 import express from "express";
 import cdnRoutes from "./routes/cdnRoutes.js";
+import cacheRoutes from "./routes/cacheRoutes.js";
 
 const app = express();
+app.use(express.json());
 
 app.use("/cdn", cdnRoutes);
+app.use("/cache", cacheRoutes);
 
 export default app;
---
+++ edgeserver1/src/controllers/cacheController.js
@@ -0,0 +1,28 @@
+import fs from "fs";
+import path from "path";
+
+import { getCachePath } from "../utils/cachePath.js";
+
+export const invalidateCache = async(req, res) =>{
+    try{
+        const { projectId, filename } = req.body;
+        const cachePath = getCachePath(projectId, filename);
+        const absoluteCachePath = path.resolve(cachePath);
+        if(fs.existsSync(absoluteCachePath)){
+            fs.unlinkSync(absoluteCachePath);
+            console.log(`🗑️ Deleted cache: ${filename}`);
+        }else{
+            console.log(`ℹ️ Cache not found: ${filename}`);
+        }
+        return res.json({
+            success:true,
+            message: "Cache invalidated",
+        })
+    }catch(error){
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: "Failed to invalidate cache",
+        });
+    }
+}
