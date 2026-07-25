f6c6a9bbe3ab2de91bb95b421da69b58c6c31d9b
Rahul Kumar Yadav <ry573870@gmail.com>
2026-07-25 08:55:46 +0530

    region logic implement

--- backend/src/utils/edges.js
+++ backend/src/utils/edges.js
@@ -1,4 +1,12 @@
 export const EDGES = [
-    "http://localhost:5001",
-    "http://localhost:5002",
+    {
+        name: "Asia Edge",
+        region: "asia",
+        url: "http://localhost:5001",
+    },
+    {
+        name: "Europe Edge",
+        region: "europe",
+        url: "http://localhost:5002",
+    },
 ];
