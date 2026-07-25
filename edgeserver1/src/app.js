da1277e54ed90499e10ce432b34b7cda313ecf0c
Rahul Kumar Yadav <ry573870@gmail.com>
2026-07-25 09:25:04 +0530

    complete

--- edgeserver1/src/app.js
+++ edgeserver1/src/app.js
@@ -1,11 +1,12 @@
 import express from "express";
 import cdnRoutes from "./routes/cdnRoutes.js";
 import cacheRoutes from "./routes/cacheRoutes.js";
+import healthRoutes from "./routes/healthRoutes.js";
 
 const app = express();
 app.use(express.json());
 
 app.use("/cdn", cdnRoutes);
 app.use("/cache", cacheRoutes);
-
+app.use("/health", healthRoutes);
 export default app;
